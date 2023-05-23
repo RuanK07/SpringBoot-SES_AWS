2412393310d9d68b71dddb0f181235e0429ce881
RuanK07 <ruank07@gmail.com>
2023-05-23 11:02:56 -0300

    Model Email

---
+++ email/src/main/java/com/ruankennedy/cliente/model/Email.java
@@ -0,0 +1,19 @@
+package com.ruankennedy.cliente.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Email {
+	
+	private String from;
+	private String to;
+	private String subject;
+	private String text;
+
+}
